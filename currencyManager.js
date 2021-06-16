69d31d4025ac60c3faf1c3338d8db8112e92707f
Paul N. Ingelmo <paulingelmo@gmail.com>
2021-06-16 08:50:16 -0400

    Added format display and data exporting, and enabled creating, editing, and deleting

--- currencyManager.js
+++ currencyManager.js
@@ -3,12 +3,68 @@ const app = angular.module('currencyManagerApp', [])
 app.controller(
   'currencyManagerController',
   function ($scope, currencySettingsFactory) {
-    $scope.selectedItem = undefined
+    $scope.selectedSetting = undefined
+    $scope.formatDisplay = undefined
+
+    $scope.editPrep = false
+    $scope.editId = undefined
 
     $scope.currentSort = 'id'
     $scope.sortOrientationAscending = true
 
     $scope.currencySettings = currencySettingsFactory.getSettings()
+    clearFields()
+
+    function applySetting(price) {
+      if (!$scope.selectedSetting) {
+        return undefined
+      }
+
+      const stringPrice = price.toString()
+      const dollarAmount = stringPrice
+        .split($scope.selectedSetting.delimiter === ',' ? '.' : ',')
+        .shift()
+        .replaceAll(
+          $scope.selectedSetting.delimiter === ',' ? '.' : ',',
+          $scope.selectedSetting.delimiter
+        )
+
+      const centsAmount = stringPrice
+        .split($scope.selectedSetting.delimiter === ',' ? '.' : ',')
+        .pop()
+
+      let formatString = ''
+
+      if ($scope.selectedSetting.symbolPosition === 'start') {
+        formatString += $scope.selectedSetting.currencySymbol
+      }
+
+      formatString += dollarAmount
+
+      if ($scope.selectedSetting.displayCents) {
+        formatString +=
+          ($scope.selectedSetting.delimiter === ',' ? '.' : ',') + centsAmount
+      }
+
+      if ($scope.selectedSetting.symbolPosition === 'end') {
+        formatString += $scope.selectedSetting.currencySymbol
+      }
+
+      if ($scope.selectedSetting.currencyCode) {
+        formatString += $scope.selectedSetting.currencyCode
+      }
+
+      return formatString
+    }
+
+    function clearFields() {
+      $scope.country = undefined
+      $scope.currencyCode = undefined
+      $scope.currencySymbol = undefined
+      $scope.symbolPosition = 'start'
+      $scope.delimiter = ','
+      $scope.displayCents = 'false'
+    }
 
     $scope.sortTable = function (sortType) {
       switch (sortType) {
@@ -42,13 +98,72 @@ app.controller(
       $scope.sortOrientationAscending = !$scope.sortOrientationAscending
     }
 
-    $scope.createSetting = function (data) {}
+    $scope.setSelectedItem = function (setting) {
+      $scope.selectedSetting = setting
+      $scope.formatDisplay = applySetting('1,234.56')
+    }
 
-    $scope.editSetting = function (data) {}
+    $scope.createSetting = function () {
+      const settingData = {
+        country: $scope.country,
+        currencyCode: $scope.currencyCode,
+        currencySymbol: $scope.currencySymbol,
+        symbolPosition: $scope.symbolPosition,
+        delimiter: $scope.delimiter,
+        displayCents: $scope.displayCents
+      }
+
+      currencySettingsFactory.createSetting(settingData)
+      clearFields()
+    }
+
+    $scope.prepEditSetting = function (id) {
+      const settingData = $scope.currencySettings.find(
+        (setting) => setting.id === id
+      )
+
+      if (!settingData) {
+        return
+      }
+
+      $scope.editId = id
+
+      $scope.country = settingData.country
+      $scope.currencyCode = settingData.currencyCode
+      $scope.currencySymbol = settingData.currencySymbol
+      $scope.symbolPosition = settingData.symbolPosition
+      $scope.delimiter = settingData.delimiter
+      $scope.displayCents = settingData.displayCents.toString()
+
+      $scope.editPrep = true
+    }
+
+    $scope.cancelEditSetting = function () {
+      clearFields()
+      $scope.editPrep = false
+    }
+
+    $scope.editSetting = function (id) {
+      const settingData = {
+        country: $scope.country,
+        currencyCode: $scope.currencyCode,
+        currencySymbol: $scope.currencySymbol,
+        symbolPosition: $scope.symbolPosition,
+        delimiter: $scope.delimiter,
+        displayCents: $scope.displayCents
+      }
+
+      currencySettingsFactory.editSetting(id, settingData)
+      clearFields()
+      $scope.editPrep = false
+    }
 
     $scope.deleteSetting = function (id) {
       currencySettingsFactory.deleteSetting(id)
-      $scope.currencySettings = currencySettingsFactory.getSettings()
+    }
+
+    $scope.exportSettings = function () {
+      currencySettingsFactory.exportSettings()
     }
   }
 )
--- currencySettings.js
+++ currencySettings.js
@@ -2,13 +2,13 @@ angular
   .module('currencyManagerApp')
   .factory('currencySettingsFactory', function () {
     // const settings = []
-    const settings = [
+    let settings = [
       {
         id: 1,
         country: 'United States',
-        currencyCode: 'USD',
+        currencyCode: '',
         currencySymbol: '$',
-        symbolPosition: 'Start',
+        symbolPosition: 'start',
         delimiter: ',',
         displayCents: true
       },
@@ -17,7 +17,7 @@ angular
         country: 'Argentina',
         currencyCode: 'USD',
         currencySymbol: '',
-        symbolPosition: 'Start',
+        symbolPosition: 'start',
         delimiter: ',',
         displayCents: false
       }
@@ -36,38 +36,73 @@ angular
     function createSetting(data) {
       settings.push({
         ...data,
-        currencyCode: data.currencyCode.toUpperCase(),
+        currencyCode:
+          typeof data.currencyCode === 'string'
+            ? data.currencyCode.toUpperCase()
+            : data.currencyCode,
         displayCents: data.displayCents === 'true' ? true : false,
         id: incrementor
       })
       incrementor++
+
+      return true
     }
 
     function editSetting(id, data) {
-      const index = settings.findIndex((elem) => {
-        elem.id === id
+      const index = settings.findIndex((setting) => {
+        setting.id === id
       })
-
+      console.log(index, settings[index])
       settings[index] = {
         ...settings[index],
         ...data,
         currencyCode: data.currencyCode.toUpperCase(),
         displayCents: data.displayCents === 'true' ? true : false
       }
+      console.log(settings[index])
+      return true
     }
 
     function deleteSetting(id) {
-      const index = settings.findIndex((elem) => {
-        elem.id === id
+      const index = settings.findIndex((setting) => {
+        setting.id === id
       })
+      console.log(index, settings[index])
 
-      settings[index].pop()
+      settings.splice(index, 1)
+
+      return true
+    }
+
+    function exportSettings() {
+      const data = 'data:text/csv;charset=utf-8,'
+      const header = [
+        'Country',
+        'Currency Code',
+        'Currency Symbol',
+        'Symbol Position',
+        'Delimiter',
+        'Display Cents'
+      ]
+      const csvContent =
+        data +
+        header +
+        '\n' +
+        settings
+          .map((setting) => {
+            return `${setting.country}, ${setting.currencyCode}, ${setting.currencySymbol}, ${setting.symbolPosition}, "${setting.delimiter}", ${setting.displayCents}`
+          })
+          .join('\n')
+      link = document.getElementById('export-link')
+      link.setAttribute('href', encodeURI(csvContent))
+      link.setAttribute('download', 'currency-settings.csv')
     }
 
     return {
       getSettings: getSettings,
       createSetting: createSetting,
       editSetting: editSetting,
-      deleteSetting: deleteSetting
+      deleteSetting: deleteSetting,
+      exportSettings: exportSettings
     }
   })
